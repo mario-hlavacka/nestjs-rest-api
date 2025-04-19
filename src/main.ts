a574006b755dfd43c7efec7a84efa0aa33184eb4
Mario Hlavacka <mario.hlavacka@gmail.com>
2025-04-19 20:03:13 +0200

    feat: add swagger api doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Shelter contributions API')
+  .setDescription('Simple REST API of shelters and contributions')
+  .setVersion('1.0')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
