67a923f7daa009fe2dd9b7c14041b533ac6d3e72
Mario Hlavacka <mario.hlavacka@gmail.com>
2025-05-04 13:56:38 +0200

    feat: add health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { SheltersModule } from './shelters/shelters.module';
 import { ContributionsModule } from './contributions/contributions.module';
 import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [SheltersModule, ContributionsModule, ConfigModule.forRoot()],
+  imports: [SheltersModule, ContributionsModule, HealthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [],
 })
